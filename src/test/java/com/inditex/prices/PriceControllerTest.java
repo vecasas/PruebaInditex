97d820f49337d58efbcb03728bc52264a6cf0adb
Víctor Casas <vecasas@gmail.com>
2025-05-12 19:08:56 +0200

    0.0.1: Actualización de dependencias

--- src/test/java/com/inditex/prices/PriceControllerTest.java
+++ src/test/java/com/inditex/prices/PriceControllerTest.java
@@ -1,4 +1,68 @@
 package com.inditex.prices;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class PriceControllerTest {
+    private final String URL = "/api/prices";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void test1() throws Exception {
+        mockMvc.perform(get(URL)
+                        .param("date", "2020-06-14-10.00.00")
+                        .param("productId", "35455")
+                        .param("brandId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(35.50));
+    }
+
+    @Test
+    void test2() throws Exception {
+        mockMvc.perform(get(URL)
+                        .param("date", "2020-06-14-16.00.00")
+                        .param("productId", "35455")
+                        .param("brandId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.priceList").value(2))
+                .andExpect(jsonPath("$.price").value(25.45));
+    }
+
+    @Test
+    void test3() throws Exception {
+        mockMvc.perform(get(URL)
+                        .param("date", "2020-06-14-21.00.00")
+                        .param("productId", "35455")
+                        .param("brandId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(35.50));
+    }
+
+    @Test
+    void test4() throws Exception {
+        mockMvc.perform(get(URL)
+                        .param("date", "2020-06-15-10.00.00")
+                        .param("productId", "35455")
+                        .param("brandId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(30.50));
+    }
+
+    @Test
+    void test5() throws Exception {
+        mockMvc.perform(get(URL)
+                        .param("date", "2020-06-16-21.00.00")
+                        .param("productId", "35455")
+                        .param("brandId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(38.95));
+    }
 }
+
+
